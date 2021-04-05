aba6df5ceb1252d88f310327665f56ebebdb79d6
JosiahCastillo <47875741+JosiahCastillo@users.noreply.github.com>
2021-04-05 03:57:22 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,32 +5,39 @@
 int main(){
     //creates a Trie
     JTrie myTrie;
-    std::string mystring = "hello";
     
-    //inserting hello
-    myTrie.insert(mystring);
-
-    mystring = "hand";
-
-    //inserting hand
-    myTrie.insert(mystring);
-
-    mystring = "helps";
-
-    //inserting helps
-    myTrie.insert(mystring);
+    //setting up strings to insert
+    std::string myStrings[3];
+    std::string myStrings[0] = "hello";
+    std::string myStrings[1] = "helps";
+    std::string myStrings[2] = "hand";
     
+    //inserting strings
+    for(int i = 0; i < 3; i++{
+        myTrie.insert(myStrings[i]);
+    }
     
-    std::cout << myTrie.search(mystring) << std::endl;
-
-    std::cout << "remove called" << std::endl;
-    myTrie.remove(mystring);
-    std::cout << "remove finished" << std::endl;
-
-
-    std::cout << myTrie.search(mystring) << std::endl;
-
-    mystring = "greatness";
-    std::cout << myTrie.search(mystring) << std::endl;
+    //confirming they exist
+    for(int i = 0; i < 3; i++){
+        if(myTrie.search(myStrings[i])){
+            std::cout << myStrings[i] << " exists." << std::endl;
+        } else {
+            std::cout << myStrings[i] << " doesn't exist." << std::endl;
+        }
+    }
+    
+    //removing "helps"
+    myTrie.remove(myStrings[1]);
+    std::cout << myStrings[1] << " removed." << std::endl;
+        
+    //confirming new trie
+    for(int i = 0; i < 3; i++){
+        if(myTrie.search(myStrings[i])){
+            std::cout << myStrings[i] << " exists." << std::endl;
+        } else {
+            std::cout << myStrings[i] << " doesn't exist." << std::endl;
+        }
+    }
+    
     return 0;
 }
